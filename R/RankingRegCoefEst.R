f7d7fa7db33d8d49b12fdef233ec08ffb79e1dd8
Nicholas Henderson <nchender@umich.edu>
2025-08-23 21:39:58 -0400

    added function for reg coefficient estimates

--- R/RankingRegCoefEst.R
+++ R/RankingRegCoefEst.R
@@ -1,20 +1,20 @@
 RankingRegCoefEst <- function(y, X, ses, tau.sq, H=1) {
 
-Q=function(beta, H=1) {
+Q=function(beta, H=H) {
   resids <- as.numeric(Y - X%*%beta)
-  B <- tausq/(ses + tau.sq)
+  B <- tau.sq/(ses + tau.sq)
   VV <- sqrt(B/(2*ses + tau.sq))
   if(H==1) {
     term1 <- sqrt(tau.sq/(2*pi))*mean(VV*dnorm(VV*resids))
   } else if(H==3) {
     der3 <- pnormder(VV*resids, ord=3)
-    f1 <- sqrt(tausq/(2*pi))
+    f1 <- sqrt(tau.sq/(2*pi))
     term1 <- 0.5*f1*mean(VV*dnorm(VV*resids)) - (1/6)*tau.sq*f1*mean((VV^3)*der3)
   } else if(H==5) {
     der1 <- pnormder(VV*resids, ord=1)
     der3 <- pnormder(VV*resids, ord=3)
     der5 <- pnormder(VV*resids, ord=5)
-    f1 <- sqrt(tausq.hat/(2*pi))
+    f1 <- sqrt(tau.sq/(2*pi))
     term1 <- (5/8)*f1*mean(VV*der1) + (1/12)*f1*tau.sq*mean((VV^3)*der3) + (1/40)*f1*tau.sq*tau.sq*mean((VV^5)*der5)
   }
   midrank_discr <- pnorm(VV*resids) - 0.5
